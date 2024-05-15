e3d27a70e31db5de081122d5edb35225649146b0
Chris J. Karr <chris@audacious-software.com>
2024-05-15 01:28:42 +0000

    Initial commit

---
+++ bash_scripts/sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# To use, update paths and add to user's local crontab:
+# */5 * * * *   /var/www/git_repos/illinoiscovid_sync.sh
+
+cd /var/www/html/www.webmunk.org$
+
+if [[ $(git pull origin deploy 2>/dev/null) != *"Already up-to-date"* ]]; then
+	echo "Updated site."
+fi
+
+
