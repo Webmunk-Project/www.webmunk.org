d19c0431f801e47124903230cdf76692adcad9f6
Chris J. Karr <chris@bric.digital>
2025-03-18 16:19:00 +0000

    Updating sync script location

--- bash_scripts/sync.sh
+++ bash_scripts/sync.sh
@@ -3,7 +3,7 @@
 # To use, update paths and add to user's local crontab:
 # */5 * * * *   /var/www/html/www.webmunk.org/bash_scripts/sync.sh
 
-cd /var/www/html/www.webmunk.org
+cd /var/www/docker/webmunk.org/www.webmunk.org
 
 if [[ $(git pull origin deploy 2>/dev/null) != *"Already up-to-date"* ]]; then
 	echo "Updated site."
